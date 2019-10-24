c3526a08f3a8ba35e6286fda5d8f42872f9ac5f1
Allister Beharry <allister.beharry@gmail.com>
2019-10-24 14:32:01 -0400

    Add E.D.D.I API client.

---
+++ src/CUI/Victor.CUI.EDDI/Client.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Victor.CUI.EDDI
+{
+    public partial class Client
+    {
+        //partial void UpdateJsonSerializerSettings(Newtonsoft.Json.JsonSerializerSettings settings);
+        //partial void PrepareRequest(System.Net.Http.HttpClient client, System.Net.Http.HttpRequestMessage request, string url);
+        //partial void PrepareRequest(System.Net.Http.HttpClient client, System.Net.Http.HttpRequestMessage request, System.Text.StringBuilder urlBuilder);
+        partial void ProcessResponse(System.Net.Http.HttpClient client, System.Net.Http.HttpResponseMessage response)
+        {
+
+        }
+
+    }
+}
