0085afa84807a980e85d9784935d1af2e7865c83
Allister Beharry <allister.beharry@gmail.com>
2019-10-31 18:59:33 -0400

    Use train_model Python function.

--- src/NLU/Victor.NLU.Snips.Train/Train.py
+++ src/NLU/Victor.NLU.Snips.Train/Train.py
@@ -1,4 +1,7 @@
+import sys
 import io
+from io import TextIOWrapper, BytesIO
+from contextlib import redirect_stdout
 import json
 
 from snips_nlu.cli import training
@@ -7,13 +10,14 @@ from snips_nlu import SnipsNLUEngine
 from snips_nlu.default_configs import CONFIG_EN
 
 
-generate_dataset.generate_dataset("en", "..\Victor.NLU.Snips\Datasets\Hello.yaml")
-#training.train("..\Victor.NLU.Snips\Datasets\Hello.yaml", output_path="..\Victor.NLU.Snips\Datasets\Hello.json")
+def train_model(input):
+    output = input.replace(".yaml", ".json")
+    with TextIOWrapper(io.FileIO(output, "wb"), sys.stdout.encoding) as buf, redirect_stdout(buf):
+        generate_dataset.generate_dataset("en", input)
+        buf.flush()
+        buf.close()
 
-engine = SnipsNLUEngine(config=CONFIG_EN)
-
-
-with io.open("..\Victor.NLU.Snips\Datasets\Hello.json") as f:
-    dataset = json.load(f)
-
-engine.fit(dataset)
+    engine = SnipsNLUEngine(config=CONFIG_EN)
+    with io.open(output) as f:
+        dataset = json.load(f)
+    engine.fit(dataset)
