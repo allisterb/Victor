a3d27b095380e77c89f1e53fe52d7bf19ebabaad
Allister Beharry <allister.beharry@gmail.com>
2019-10-31 19:34:20 -0400

    Persist model to directory. Add hello intent.

--- src/NLU/Victor.NLU.Snips.Train/Train.py
+++ src/NLU/Victor.NLU.Snips.Train/Train.py
@@ -1,5 +1,6 @@
 import sys
 import io
+import os
 from io import TextIOWrapper, BytesIO
 from contextlib import redirect_stdout
 import json
@@ -9,15 +10,19 @@ from snips_nlu.cli import generate_dataset
 from snips_nlu import SnipsNLUEngine
 from snips_nlu.default_configs import CONFIG_EN
 
+engine = SnipsNLUEngine(config=CONFIG_EN)
 
-def train_model(input):
+def train_model(_input, name):
+    input = os.path.join("..", "Victor.NLU.Snips", "Datasets", _input)
     output = input.replace(".yaml", ".json")
     with TextIOWrapper(io.FileIO(output, "wb"), sys.stdout.encoding) as buf, redirect_stdout(buf):
         generate_dataset.generate_dataset("en", input)
         buf.flush()
         buf.close()
-
-    engine = SnipsNLUEngine(config=CONFIG_EN)
+    
     with io.open(output) as f:
         dataset = json.load(f)
     engine.fit(dataset)
+    engine.persist(os.path.join(os.path.dirname(input), "..", "Engines", name))
+
+    
