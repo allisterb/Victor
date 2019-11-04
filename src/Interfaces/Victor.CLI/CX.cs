c32792dcca525f6bf4d335804f03001a1feea444
Allister Beharry <allister.beharry@gmail.com>
2019-11-04 01:13:32 -0400

    Hookup NLU and ASR.

--- src/Interfaces/Victor.CLI/CX.cs
+++ src/Interfaces/Victor.CLI/CX.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Drawing;
+using System.IO;
 using System.Text;
 using System.Threading;
 using System.Diagnostics;
@@ -20,21 +21,20 @@ namespace Victor.CLI
         {
             Options = options;
             Client = new EDDIClient("http://eddi-evals25-shared-7daa.apps.hackathon.rhmi.io", HttpClient);
-            
+            GeneralNLU = new SnipsNLUEngine(Path.Combine("Engines", "general"));
+            JuliusSession = new JuliusSession();
+            JuliusSession.Recognized += JuliusSession_Recognized;
+            Initialized = GeneralNLU.Initialized && JuliusSession.Initialized;
         }
 
-        private void JuliusSession_Recognized(string sentence)
-        {
-            throw new NotImplementedException();
-        }
         #endregion
 
         #region Properties
         public CXOptions Options { get; }
         protected EDDIClient Client { get;  }
-        protected JuliusSession JuliusSession { get; } = new JuliusSession();
+        protected JuliusSession JuliusSession { get; }
 
-        //protected SnipsNLUEngine GeneralNLU = new SnipsNLUEngine("victorcx_general");
+        protected SnipsNLUEngine GeneralNLU { get; }
 
         protected Tuple<DateTime, string> Context { get; set; }
 
@@ -44,19 +44,31 @@ namespace Victor.CLI
         #region Methods
         public void Start()
         {
-            Sc.Clear();
+            ThrowIfNotInitialized();
             Sc.Beep();
             Sc.Beep();
             
             WriteLine("Welcome to Victor CX");
             SetContext("Welcome");
+            JuliusSession.Start();
             Prompt();
+            JuliusSession.Stop();
+        }
+
+        public void HandleInput(DateTime time, string input)
+        {
+            GeneralNLU.GetIntents(input, out string[] intents, out string json, out string error);
+            ReadLine.Send(json);
         }
 
         public void WriteLine(string s) => Co.WriteLine(s, Color.PaleGoldenrod);
         
         public string Prompt() => ReadLine.Read("|> ");
 
+        #endregion
+
+        #region Event Handlers
+        private void JuliusSession_Recognized(string sentence) => HandleInput(DateTime.Now, sentence);
         #endregion
     }
 }
