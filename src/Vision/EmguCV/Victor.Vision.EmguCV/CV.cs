95d18646ffa51a7bcd0f29c871cb64dba7b5a2e5
Allister Beharry <allister.beharry@gmail.com>
2020-11-09 23:34:39 -0400

    Add EmguCV project.  Can capture camera frame.

---
+++ src/Vision/EmguCV/Victor.Vision.EmguCV/CV.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Emgu.CV;
+//using Emgu.CV.UI;
+using Emgu.CV.Structure;
+using System.Drawing;
+
+namespace Victor.Vision
+{
+    public class CV
+    {
+        public static void CaptureCamera()
+        {
+            VideoCapture c = new VideoCapture();
+            var v = c.QueryFrame();
+            v.Save("capture.png");
+          
+            //Emu.CV.Capture capture = new Capture(); //create a camera captue
+            //Application.Idle += new EventHandler(delegate (object sender, EventArgs e)
+            //{  //run this until application closed (close button click on image viewer)
+            //    viewer.Image = capture.QueryFrame(); //draw the image obtained from camera
+            //});
+            //viewer.ShowDialog(); //show the image viewer
+        }
+    }
+}
--- tests/Victor.Tests.FN/UnitTest1.cs
+++ tests/Victor.Tests.FN/UnitTest1.cs
@@ -5,20 +5,26 @@ using System.Globalization;
 
 using Xunit;
 
+using Victor.Vision;
 namespace Victor.Tests
 {
     public class MSSpeechTests
     {
       
-
         [Fact]
-        public void Test1()
+        public void CanCapture()
+        {
+            CV.CaptureCamera();
+            Assert.True(true);
+        }
+        
+        [Fact]
+        public void CanSpeak()
         {
             SpeechSynthesizer ss = new SpeechSynthesizer();
             ss.SetOutputToDefaultAudioDevice();
             ss.Speak("I am awake");
             Assert.NotNull(ss);
-            //Assert.Equal(3, 3);
         }
     }
 }
