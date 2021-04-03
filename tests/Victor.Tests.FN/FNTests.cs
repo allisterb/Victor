538e6e88b7b3faeec37c070c61fbe641873bd0d3
Allister Beharry <allister.beharry@gmail.com>
2021-04-03 15:59:25 -0400

    Add ExecuteJournalAction method to NLedger.

--- tests/Victor.Tests.FN/FNTests.cs
+++ tests/Victor.Tests.FN/FNTests.cs
@@ -4,9 +4,17 @@ using System.Text;
 
 using Xunit;
 
+using NLedger;
+
+using NLedger.Accounts;
+using NLedger.Amounts;
+using NLedger.Expressions;
+using NLedger.Journals;
+using NLedger.Textual;
+using NLedger.Values;
+using NLedger.Xacts;
 using NLedger.Abstracts.Impl;
 using NLedger.Utility.ServiceAPI;
-
 namespace Victor.Tests
 {
     public class FNTests
@@ -29,17 +37,35 @@ namespace Victor.Tests
         public void CanStartNLedger()
         {
             var engine = new ServiceEngine(
-                configureContext: context => { context.IsAtty = true; },
+                configureContext: context => { context.IsAtty = true; context.Logger = new NLedger.Utils.Logger(); },
                 createCustomProvider: mem =>
                 {
                     mem.Attach(w => new MemoryAnsiTextWriter(w));
                     return null;
                 });
-            var session = engine.CreateSession("-f /dev/stdin", InputText);
+            var session = engine.CreateSession("-f ledger.dat");
+            var journal = session.GlobalScope.Session.Journal;
             Assert.True(session.IsActive);
-
+           
             var response = session.ExecuteCommand("bal checking --account=code");
+
+            session.ExecuteJournalAction((j) =>
+            {
+                Xact xact = new Xact();
+                xact.AddPost(new Post(journal.Master, new Amount(10, new NLedger.Commodities.Commodity(session.MainApplicationContext.CommodityPool, new NLedger.Commodities.CommodityBase("$")))));
+                xact.AddPost(new Post(journal.Master, new Amount(-10, new NLedger.Commodities.Commodity(session.MainApplicationContext.CommodityPool, new NLedger.Commodities.CommodityBase("$")))));
+                j.AddXact(xact);
+            });
+
+            
+            // response = session.ExecuteCommand("bal checking --account=code");
+            //session.ExecuteCommand
+            //j
+            response = session.ExecuteCommand("bal checking --account=code");
+            //session.Dispose();
             Assert.False(response.HasErrors);
+            Assert.Equal(2, session.GlobalScope.Session.Journal.Xacts.Count);
+            //session.ServiceEngine.
             //Assert.Equal(BalCheckingOutputText.Replace("\r", "").Trim(), response.OutputText.Trim());
         }
     }
